cc679ea85942bab30a69d1d92925f4f6a8e08d8a
leofvo <leofvo@proton.me>
2023-02-22 10:21:53 +0100

    build: reusable template
    Signed-off-by: leofvo <leofvo@proton.me>

--- template/node-19/function/handler.js
+++ template/node-19/function/handler.js
@@ -1,6 +1,13 @@
-const randomAnimalName = require("random-animal-name");
-
 exports.handler = async function (req, res) {
-  console.log(`Random animal name : ${randomAnimalName()}`);
-  return res.status(200).send("My first function !");
+  // "req" is an Express request object
+  // See: https://expressjs.com/en/api.html#req.app
+  console.log(req.body);
+
+  // "res" is an Express response object
+  // See: https://expressjs.com/en/api.html#res.app
+  console.log(res);
+
+  // Here you have full control on the response.
+  // You can return anything you want, but you always need to set at least the response code
+  return res.status(200).send('My first function !');
 };
