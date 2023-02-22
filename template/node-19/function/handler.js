95de3a78ff9f5d08b614014c7b3143a7923fa994
thomasgouveia <gouveia.thomas@outlook.fr>
2023-02-22 21:39:06 +0000

    fix(node-19): update runtime to working state

--- template/node-19/function/handler.js
+++ template/node-19/function/handler.js
@@ -1,13 +1,14 @@
 exports.handler = async function (req, res) {
+  console.log("I can log some messages...")
   // "req" is an Express request object
   // See: https://expressjs.com/en/api.html#req.app
-  console.log(req.body);
+  // console.log(req);
 
   // "res" is an Express response object
   // See: https://expressjs.com/en/api.html#res.app
-  console.log(res);
+  // console.log(res);
 
   // Here you have full control on the response.
   // You can return anything you want, but you always need to set at least the response code
-  return res.status(200).send('My first function !');
+  return res.status(200).send(JSON.stringify('My first function !'));
 };
